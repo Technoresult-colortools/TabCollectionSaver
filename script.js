a1e49bd5b44342b36a97d44a58c955b18bb1ea76
Technoresult-colortools <technoresult@gmail.com>
2024-09-05 14:25:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const mobileMenuButton = document.querySelector('.mobile-menu-button');
+        const navLinks = document.querySelector('.nav-links');
+
+        mobileMenuButton.addEventListener('click', function () {
+            navLinks.classList.toggle('open');
+        });
+    });
